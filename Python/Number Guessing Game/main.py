f07e1ebd2d76cc029bd47ef191093699ee1fc51c
Manash Pratim Kalita <manashpk05@gmail.com>
2022-10-17 20:04:10 +0530

    Comments added

--- Python/Number Guessing Game/main.py
+++ Python/Number Guessing Game/main.py
@@ -1,6 +1,9 @@
+# Number guessing game using python
+
+
 import time
 
-number = 78
+number = 78         # The number selected for the game
 count = 1
 print("\n\t*** WELCOME TO THE NUMBER GUESSING GAME ***\n")
 print("You will be given 10 chances to guess the number correctly.\n"
@@ -18,17 +21,17 @@ while count <= 9:
         print("Hurrah...you guessed the number correctly in", count, "guesses. You won. ")
         break
     print("No. of guesses left: ", 10 - count, "\n")
-    time.sleep(0.4)
+    time.sleep(0.4)         # Program sleeps for 0.4 seconds
 
-    count = count + 1
+    count += 1              # Increment of count by 1
 
 if count == 10:
     print(count, end="")
     guess = int(input(". Guess the number: "))
     if guess == number:
-        print("Hurrah...you guessed the number correctly in", count, "guesses. You won. ")
+        print("Hurrah...you guessed the number correctly in", count, "guesses. You won. ")  # Win message
     else:
-        print("Oops...you have used all your chances.\n"
+        print("Oops...you have used all your chances.\n"                                    # Lose message
               "The number was", number)
         print("Better luck next time.")
 
